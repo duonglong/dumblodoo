150fd86060b509a7477a6673f91b80642384cf8a
Long Duong <duongthanhlong93@gmail>
2022-09-21 18:07:33 +0700

    [WIP] Alert Management

---
+++ alert_management/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Alert Management',
+    'version': '1.2',
+    'summary': 'Alert Management',
+    'sequence': 10,
+    'description': """
+Alert management
+====================
+TO_FILL_IN
+    """,
+    'category': 'hidden',
+    'website': 'https://apps.odoo.com/apps/modules/15.0/alert_configuration/',
+    'images': [],
+    'depends': ['base'],
+    'data': [
+        'views/alert_configuration.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'assets': {
+
+    },
+    'license': 'LGPL-3',
+}
---
+++ alert_management/models/alert_configuration.py
@@ -0,0 +1,26 @@
+from odoo import models, api, fields, _
+
+OPERATORS = [
+    ('=', '='),
+    ('>', '>'),
+    ('<', '<'),
+    ('>=', '>='),
+    ('<=', '<='),
+]
+
+THRESHOLD_TYPE = [
+    ('amount', 'Amount'),
+    ('percentage', 'Percentage')
+]
+
+
+class AlertConfiguration(models.Model):
+    _name = 'alert.configuration'
+
+    name = fields.Char(string=_("Name"), required=True)
+    model_id = fields.Many2one(string=_("Object"), comodel_name='ir.model.model')
+    left_field_id = fields.Many2one(string=_("Left"), comodel_name="ir.model.fields")
+    right_field_id = fields.Many2one(string=_("Right"), comodel_name="ir.model.fields")
+    operator = fields.Selection(string=_("Operator"), selection=OPERATORS)
+    threshold_type = fields.Selection(string=_("Threshold Type"), selection=THRESHOLD_TYPE)
+    threshold_value = fields.Float(string=_("Threshold"))
